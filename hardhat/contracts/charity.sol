e3c57e9a59a338a62edebed0fbb7c33b37f1c0d5
Tumelo <mashabaindustriesllc@gmail.com>
2022-07-20 02:24:19 +0200

    initial commit

---
+++ hardhat/contracts/charity.sol
@@ -0,0 +1,134 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract charity {
+    constructor() {}
+
+    struct Cause {
+        uint256 id;
+        address person;
+        uint256 goal;
+    }
+
+    struct Donor {
+        uint256 jd;
+        address donor;
+        uint256 amount;
+    }
+
+    event CauseListed(uint256 id, address person, uint256 goal);
+
+    event CauseCancelled(uint256 id, address person);
+
+    event GoalHit(uint256 id, address person, uint256 goal);
+
+    event Donated(uint256 jd, address donor, uint256 amount);
+
+    event ProceedsWithdrawn(address person, uint256 proceeds);
+
+    mapping(address => mapping(uint256 => Donor)) private donors;
+    mapping(address => mapping(uint256 => Cause)) private listings;
+    mapping(address => uint256) private proceeds;
+    mapping(address => uint256) private donation;
+
+    modifier notListed(uint256 id, address person) {
+        Cause memory listing = listings[person][id];
+        require(listing.goal > 0, "Already Listed");
+        _;
+    }
+
+    modifier isListed(uint256 id, address person) {
+        Cause memory listing = listings[person][id];
+        require(listing.goal <= 0, "Not Listed");
+        _;
+    }
+
+    modifier hasDonated(uint256 jd, address donor) {
+        Donor memory dons = donors[donor][jd];
+        require(dons.amount <= 0, "Has Not Donated to Cause");
+        _;
+    }
+
+    modifier isOwner(uint256 id, address person) {
+        Cause memory listing = listings[person][id];
+        address owner = listing.person;
+        require(msg.sender == owner, "Not Owner");
+        _;
+    }
+
+    function listCause(
+        uint256 id,
+        address person,
+        uint256 goal
+    ) external notListed(id, msg.sender) {
+        listings[person][id] = Cause(id, person, goal);
+        emit CauseListed(id, msg.sender, goal);
+        id++;
+    }
+
+    function CancelListing(uint256 id, address person)
+        external
+        isListed(id, person)
+        isOwner(id, person)
+    {
+        delete (listings[person][id]);
+        emit CauseCancelled(id, msg.sender);
+    }
+
+    function Donate(
+        uint256 id,
+        address person,
+        uint256 jd,
+        address donor,
+        uint256 amount
+    ) external payable isListed(id, person) {
+        amount = msg.value;
+        Cause memory listedCause = listings[person][id];
+        donors[donor][jd] = Donor(jd, msg.sender, amount);
+        emit Donated(jd, msg.sender, amount);
+        proceeds[listedCause.person] += amount;
+        donation[msg.sender] += msg.value;
+        jd++;
+    }
+
+    function DonorCancelled(uint256 jd, address donor)
+        external
+        hasDonated(jd, donor)
+    {
+        donation[donor] = 0;
+        // Transfer vs call vs Send
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = donor.call{value: donation[donor]}("");
+        require(success);
+    }
+
+    function UpdateCause(
+        uint256 id,
+        address person,
+        uint256 newGoal
+    ) external isListed(id, person) isOwner(id, msg.sender) {
+        listings[person][id].goal = newGoal;
+        emit CauseListed(id, msg.sender, newGoal);
+    }
+
+    function WithdrawProceeds() external {
+        uint256 balance = proceeds[msg.sender];
+        require(balance > 0, "No proceeds");
+        balance = 0;
+        (bool success, ) = payable(msg.sender).call{value: balance}("");
+        require(success, "Transfer failed");
+        emit ProceedsWithdrawn(msg.sender, balance);
+    }
+
+    function getListing(uint256 ad, address person)
+        external
+        view
+        returns (Cause memory)
+    {
+        return listings[person][ad];
+    }
+
+    function getProceeds(address person) external view returns (uint256) {
+        return proceeds[person];
+    }
+}
